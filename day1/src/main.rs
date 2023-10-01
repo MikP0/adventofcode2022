4b5d7466e1701bee45fc447746af31dbc86ef24f
MikP0 <m.popieluch@gmail.com>
2023-10-01 18:53:25 +0200

    style: cargo fmt

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -7,7 +7,14 @@ fn main() -> std::io::Result<()> {
     file.read_to_string(&mut input)?;
 
     let elf_items: Vec<&str> = input.split("\n\n").collect();
-    let mut calories: Vec<u32> = elf_items.into_iter().map(|elf| elf.split("\n").map(|calorie_str| calorie_str.parse::<u32>().unwrap()).sum()).collect();
+    let mut calories: Vec<u32> = elf_items
+        .into_iter()
+        .map(|elf| {
+            elf.split("\n")
+                .map(|calorie_str| calorie_str.parse::<u32>().unwrap())
+                .sum()
+        })
+        .collect();
     let max = calories.iter().max().unwrap();
 
     println!("Part 1: {:?}", max);
@@ -15,7 +22,7 @@ fn main() -> std::io::Result<()> {
     calories.sort();
     calories.reverse();
 
-    let sum_of_top3: u32= calories[0..3].into_iter().sum();
+    let sum_of_top3: u32 = calories[0..3].into_iter().sum();
 
     println!("Part 2: {:?}", sum_of_top3);
 
